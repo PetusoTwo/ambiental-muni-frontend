27810e811a4cb92211245ce2e88a2b0a8127b1b9
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-03 13:04:12 -0500

    update helper para poner sin datos al denunciado

--- backend/app/Helpers/form_helper.php
+++ backend/app/Helpers/form_helper.php
@@ -1,53 +1,148 @@
 <?php
 
+// if (!function_exists('getDenounceData')) {
+//     function getDenounceData(array $generalAspects, array $previousDenounce, array $ambientalPromises, array $proof): array {
+
+//         [ $year, $day, $month ] = explode('-', $generalAspects['denounceDate']);
+    
+//         return [
+//             "code" => $generalAspects['code'],
+//             "reception_media" => $generalAspects['receptionMedia'],
+//             "date" => $year.'-'.$month.'-'.$day,
+//             "has_previous_denounce" => (int) $previousDenounce['hasPreviousDenounce'],
+//             "has_response" =>  (int) $previousDenounce['hasResponseDenounce'],
+//             "directed_entity" => $previousDenounce['directedEntity'],
+//             "entity_response" => $previousDenounce['entityResponse'],
+//             "comunication_media" => $previousDenounce['communicationMedia'],
+//             "source" => $previousDenounce['source'],
+//             "keep_identity" =>  (int) $previousDenounce['keepIdentity'],
+//             "address" => $ambientalPromises['address'],
+//             "reference" => $ambientalPromises['reference'],
+//             "facts_description" => $ambientalPromises['factsDescription'],
+//             "ambiental_promise" => $ambientalPromises['ambientalPromise'],
+//             "proof_description" => $proof['proofDescription']
+//         ];
+    
+//     }
+// }
+
+// if (!function_exists('getPersonData')) {
+//     function getPersonData($person): array {
+
+//         $isNatural = $person['isNatural'];
+//         $entity = $person['entity'];
+
+//         $legalRepresentator = $person['legalRepresentator'];
+//         $email = $person['email'];
+//         $fixedPhone = $person['fixedPhone'];
+//         $firstPhone = $person['firstPhone'];
+//         $secondPhone = $person['secondPhone'];
+
+//         return [
+//             "id_doc_type" => $isNatural ? 1 : 2,
+//             "is_natural_person" => (int) $isNatural,
+//             "doc_number" => $isNatural ? $entity['dni'] : $entity['ruc'],
+//             "trade_name" => $isNatural ? 'NO TIENE' : $entity['tradeName'],
+//             "name" => $isNatural ? $entity['name'] : "NO TIENE",
+//             "paternal_surname" => $isNatural ? $entity['paternalSurname'] : "NO TIENE",
+//             "mother_surname" => $isNatural ? $entity['motherSurname'] : "NO TIENE",
+//             "legal_representator" => $legalRepresentator === '' ? 'NO ESPECIFICA' : $legalRepresentator,
+//             "address" => $person['address'],
+//             "fixed_phone" => $fixedPhone === '' ? 'NO TIENE' : $fixedPhone,
+//             "first_phone" => $firstPhone === '' ? 'NO TIENE' : $firstPhone,
+//             "second_phone" => $secondPhone === '' ? 'NO TIENE' : $secondPhone,
+//             "email" => $email === '' ? 'NO TIENE' : $email,
+//             "departament" => "LAMBAYEQUE",
+//             "province" => "CHICLAYO",
+//             "distric" => "JOSE LEONARDO ORTIZ"
+//         ];
+
+//     }
+// }
+
+// if (!function_exists('getPersonDenounceData')) {
+//     function getPersonDenounceData(int $idDenounce, int $idPerson, int $isAffected): array {
+
+//         return [
+//             "id_person" => $idPerson,
+//             "id_denounce" => $idDenounce,
+//             "is_affected" => (int) $isAffected
+//         ];
+
+//     }
+// }
+
 if (!function_exists('getDenounceData')) {
     function getDenounceData(array $generalAspects, array $previousDenounce, array $ambientalPromises, array $proof): array {
 
-        [ $year, $day, $month ] = explode('-', $generalAspects['denounceDate']);
+        [$year, $day, $month] = explode('-', $generalAspects['denounceDate']);
     
         return [
             "code" => $generalAspects['code'],
             "reception_media" => $generalAspects['receptionMedia'],
-            "date" => $year.'-'.$month.'-'.$day,
+            "date" => $year . '-' . $month . '-' . $day,
             "has_previous_denounce" => (int) $previousDenounce['hasPreviousDenounce'],
-            "has_response" =>  (int) $previousDenounce['hasResponseDenounce'],
-            "directed_entity" => $previousDenounce['directedEntity'],
-            "entity_response" => $previousDenounce['entityResponse'],
-            "comunication_media" => $previousDenounce['communicationMedia'],
-            "source" => $previousDenounce['source'],
-            "keep_identity" =>  (int) $previousDenounce['keepIdentity'],
-            "address" => $ambientalPromises['address'],
-            "reference" => $ambientalPromises['reference'],
-            "facts_description" => $ambientalPromises['factsDescription'],
-            "ambiental_promise" => $ambientalPromises['ambientalPromise'],
-            "proof_description" => $proof['proofDescription']
+            "has_response" => (int) $previousDenounce['hasResponseDenounce'],
+            "directed_entity" => $previousDenounce['directedEntity'] ?? 'NO ESPECIFICA',
+            "entity_response" => $previousDenounce['entityResponse'] ?? 'NO ESPECIFICA',
+            "comunication_media" => $previousDenounce['communicationMedia'] ?? 'NO ESPECIFICA',
+            "source" => $previousDenounce['source'] ?? 'NO ESPECIFICA',
+            "keep_identity" => (int) $previousDenounce['keepIdentity'],
+            "address" => $ambientalPromises['address'] ?? 'NO TIENE',
+            "reference" => $ambientalPromises['reference'] ?? 'NO TIENE',
+            "facts_description" => $ambientalPromises['factsDescription'] ?? 'NO TIENE',
+            "ambiental_promise" => $ambientalPromises['ambientalPromise'] ?? 'NO TIENE',
+            "proof_description" => $proof['proofDescription'] ?? 'NO TIENE'
         ];
-    
     }
 }
 
 if (!function_exists('getPersonData')) {
     function getPersonData($person): array {
 
-        $isNatural = $person['isNatural'];
-        $entity = $person['entity'];
+        if (empty($person)) {
+            // Retorna valores seguros si no hay datos
+            return [
+                "id_doc_type" => 1,
+                "is_natural_person" => 1,
+                "doc_number" => 'SIN DATOS',
+                "trade_name" => 'SIN DATOS',
+                "name" => 'SIN DATOS',
+                "paternal_surname" => 'SIN DATOS',
+                "mother_surname" => 'SIN DATOS',
+                "legal_representator" => 'SIN DATOS',
+                "address" => 'SIN DATOS',
+                "fixed_phone" => 'SIN DATOS',
+                "first_phone" => 'SIN DATOS',
+                "second_phone" => 'SIN DATOS',
+                "email" => 'SIN DATOS',
+                "departament" => "LAMBAYEQUE",
+                "province" => "CHICLAYO",
+                "distric" => "JOSE LEONARDO ORTIZ"
+            ];
+        }
 
-        $legalRepresentator = $person['legalRepresentator'];
-        $email = $person['email'];
-        $fixedPhone = $person['fixedPhone'];
-        $firstPhone = $person['firstPhone'];
-        $secondPhone = $person['secondPhone'];
+        $isNatural = $person['isNatural'] ?? true;
+        $entity = $person['entity'] ?? [];
+
+        $legalRepresentator = $person['legalRepresentator'] ?? '';
+        $email = $person['email'] ?? '';
+        $fixedPhone = $person['fixedPhone'] ?? '';
+        $firstPhone = $person['firstPhone'] ?? '';
+        $secondPhone = $person['secondPhone'] ?? '';
 
         return [
             "id_doc_type" => $isNatural ? 1 : 2,
             "is_natural_person" => (int) $isNatural,
-            "doc_number" => $isNatural ? $entity['dni'] : $entity['ruc'],
-            "trade_name" => $isNatural ? 'NO TIENE' : $entity['tradeName'],
-            "name" => $isNatural ? $entity['name'] : "NO TIENE",
-            "paternal_surname" => $isNatural ? $entity['paternalSurname'] : "NO TIENE",
-            "mother_surname" => $isNatural ? $entity['motherSurname'] : "NO TIENE",
+            "doc_number" => $isNatural 
+                ? ($entity['dni'] ?? 'SIN DATOS') 
+                : ($entity['ruc'] ?? 'SIN DATOS'),
+            "trade_name" => $isNatural ? 'NO TIENE' : ($entity['tradeName'] ?? 'SIN DATOS'),
+            "name" => $isNatural ? ($entity['name'] ?? 'SIN DATOS') : "NO TIENE",
+            "paternal_surname" => $isNatural ? ($entity['paternalSurname'] ?? 'SIN DATOS') : "NO TIENE",
+            "mother_surname" => $isNatural ? ($entity['motherSurname'] ?? 'SIN DATOS') : "NO TIENE",
             "legal_representator" => $legalRepresentator === '' ? 'NO ESPECIFICA' : $legalRepresentator,
-            "address" => $person['address'],
+            "address" => $person['address'] ?? 'SIN DATOS',
             "fixed_phone" => $fixedPhone === '' ? 'NO TIENE' : $fixedPhone,
             "first_phone" => $firstPhone === '' ? 'NO TIENE' : $firstPhone,
             "second_phone" => $secondPhone === '' ? 'NO TIENE' : $secondPhone,
@@ -56,18 +151,15 @@ if (!function_exists('getPersonData')) {
             "province" => "CHICLAYO",
             "distric" => "JOSE LEONARDO ORTIZ"
         ];
-
     }
 }
 
 if (!function_exists('getPersonDenounceData')) {
     function getPersonDenounceData(int $idDenounce, int $idPerson, int $isAffected): array {
-
         return [
             "id_person" => $idPerson,
             "id_denounce" => $idDenounce,
             "is_affected" => (int) $isAffected
         ];
-
     }
 }
